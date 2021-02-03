ff06e3660cd5d1f792bdbdf4953736bd3d88c209
yaamh <50399700+yaamh@users.noreply.github.com>
2021-02-03 14:06:13 +0800

    Update filelist.h
    add copy cut

--- filelist.h
+++ filelist.h
@@ -35,8 +35,8 @@ dirnode_s* create_rootnode(const char* path);
 void destroy_filelist(dirnode_s *file);
 dirnode_s* get_filelist(dirnode_s *dir);
 int foreach_file(dirnode_s *dirnode,int (*filter)(void*,void*,int),void*arg);
-void* delete_file(filenode_s *node);
-void insert_file(dirnode_s *dirnode,filenode_s *node);
+void* delete_filenode(filenode_s *node);
+void insert_filenode(dirnode_s *dirnode,filenode_s *node,int bcpy);
 int insert_checkfile(dirnode_s *dirnode,filenode_s *node);
 char* get_path(filenode_s *file);
 
